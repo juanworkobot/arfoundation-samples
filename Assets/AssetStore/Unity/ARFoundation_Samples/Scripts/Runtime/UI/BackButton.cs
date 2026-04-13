3a14e93a95a9b577a1dd6a8a48f7354a5d4c264a
Juan Maldonado <juan.maldonado@workobot.com>
2026-04-13 13:41:28 -0500

    refactor(repo): AR Foundation samples organization and repo preparation

---
+++ Assets/AssetStore/Unity/ARFoundation_Samples/Scripts/Runtime/UI/BackButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace UnityEngine.XR.ARFoundation.Samples
+{
+    public class BackButton : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject m_BackButton;
+
+        [SerializeField]
+        string m_MenuSceneNameOverride;
+
+        public GameObject backButton
+        {
+            get => m_BackButton;
+            set => m_BackButton = value;
+        }
+
+        void Start()
+        {
+            if (Application.CanStreamedLevelBeLoaded(MenuLoader.GetMenuSceneName()))
+                m_BackButton.SetActive(true);
+        }
+
+        void Update()
+        {
+            // Handles Android physical back button
+            if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
+                BackButtonPressed();
+        }
+
+        public void BackButtonPressed()
+        {
+            string menuSceneName = m_MenuSceneNameOverride;
+            if (string.IsNullOrEmpty(menuSceneName) || !Application.CanStreamedLevelBeLoaded(menuSceneName))
+                menuSceneName = MenuLoader.GetMenuSceneName();
+            if (Application.CanStreamedLevelBeLoaded(menuSceneName))
+                SceneManager.LoadScene(menuSceneName, LoadSceneMode.Single);
+        }
+    }
+}
